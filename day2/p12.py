88039af0bcc80262a425129a0910ed4d8f709add
Ameensha773 <ameensha736@gmail.com>
2024-08-01 13:12:59 +0530

    update

--- day2/p12.py
+++ day2/p12.py
@@ -1,9 +1,13 @@
-# prin x shape mad up of stars
-input_number = int(input('Enter the no of lines '))
-for i in range(1,input_number +1):
-    for j in range(1,input_number +1):
-        if(i==j or i+j==6):
-            print('*',end=' ')
-        else:
-            print('',end=' ')
-    print()
+#Print X shape made up of stars of n lines
+number_of_lines = int(input("Enter the number of lines for X:  ")) #input from user
+if (number_of_lines % 2==0):
+    print('Even numbers cannot give the perfect X shape')
+    
+else: 
+    for i in range (number_of_lines):          #outer for loop for rows
+        for j in range(number_of_lines):       #inner for loop for columns
+            if(i ==j or i+j == number_of_lines-1):
+                print('*', end=' ')
+            else:
+                print(' ',end=' ')
+        print()
