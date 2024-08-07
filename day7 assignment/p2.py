19216205a3d8bae0e088c39020c4f38eb7cabe5f
Ameensha773 <ameensha736@gmail.com>
2024-08-07 13:34:31 +0530

    program updated

--- day7 assignment/p2.py
+++ day7 assignment/p2.py
@@ -1,43 +1,51 @@
 age = int(input("Enter age: "))
-gender = input("Enter gender: \nM. Male\nF. Female.")
-occupation = input("Enter occupation :\n s student \n w working  ")
-stay = input('Enter stay location : \n l locality \n h hosteller ')
+gender = input("Enter gender: \nM. Male\nF. Female.").lower()
+occupation = input("Enter occupation :\n s student \n w working  ").lower()
+stay = input('Enter stay location : \n l locality \n h hosteller ').lower()
 
-if age < 45 and gender=='f' and occupation =='w' and stay=='h':
-    print("50 rs coupon on nykaa product")
-else:
-    print('25% Discount on all products')    
-if age < 45 and gender =='f' and occupation =='s' and stay=='h':
-    print("100rs coupon on nykaa product and discount on book")
-else:
-    print('100rs Discount on all products and books') 
-
-
-if age< 60 and gender =='m' and occupation =='w' and stay=='h':
-    print('50% Discount on laptop and phones')   
-else:
-    print('25% Discount on all products')     
-if age< 60 and gender =='m' and occupation =='s' and stay=='h':
-    print('100rs Discount on titan and fasttrack')   
-else:
-    print('25% Discount on all products and books')         
-
-
-if age < 45 and gender=='f' and occupation =='w' and stay=='l':
-    print("50rs coupon on nykaa product")
-else:
-    print('25rs Discount on all products')  
-if age < 45 and gender=='f' and occupation =='s' and stay=='l':
-    print("50% Discount on nykaa product and discount on books")
-else:
-    print('25% Discount on all products and books')  
-
-
-if age< 60 and gender =='m' and occupation =='w' and stay=='l':
-    print('40% Discount on laptop and phones')   
-else:
-    print('20% Discount on all products') 
-if age< 60 and gender =='m' and occupation =='w' and stay=='l':
-    print('40% Discount on titan and fasttrack product')   
-else:
-    print('20% Discount on all products and books')             
+if gender == 'f':
+    if age < 45:
+        if occupation == 'w':
+            if stay == 'h':
+                print("50 rs coupon on nykaa product")
+            elif stay == 'l':
+                print("50rs coupon on nykaa product")
+        elif occupation == 's':
+            if stay == 'h':
+                print("100rs coupon on nykaa product and discount on book")
+            elif stay == 'l':
+                print("50% Discount on nykaa product and discount on books")
+    elif age >= 45:
+        if occupation == 'w':
+            if stay == 'h':
+                print('25% Discount on all products')
+            elif stay == 'l':
+                print('25rs Discount on all products')
+        elif occupation == 's':
+            if stay == 'h':
+                print('100rs Discount on all products and books')
+            elif stay == 'l':
+                print('25% Discount on all products and books')
+elif gender == 'm':
+    if age < 60:
+        if occupation == 'w':
+            if stay == 'h':
+                print('50% Discount on laptop and phones')
+            elif stay == 'l':
+                print('40% Discount on laptop and phones')
+        elif occupation == 's':
+            if stay == 'h':
+                print('100rs Discount on titan and fastrack')
+            elif stay == 'l':
+                print('40% Discount on titan and fasttrack product')
+    elif age >= 60:
+        if occupation == 'w':
+            if stay == 'h':
+                print('25% Discount on all products')
+            elif stay == 'l':
+                print('20% Discount on all products')
+        elif occupation == 's':
+            if stay == 'h':
+                print('25% Discount on all products and books')
+            elif stay == 'l':
+                print('20% Discount on all products and books')
